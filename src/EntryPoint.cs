6fce51adc0c8e4725266a72fe92e3e96db2c4915
primo-ppcg <mike.tryczak@gmail.com>
2017-05-05 12:59:37 +0700

    Removed Debug

--- src/EntryPoint.cs
+++ src/EntryPoint.cs
@@ -13,18 +13,7 @@ namespace ppcg {
             ArgumentParser<MyOptions> argparser = new ArgumentParser<MyOptions>();
             MyOptions options = argparser.Parse(args);
 
-            Cruncher cruncher;
-
-#if DEBUG
-            // used for profiling
-            cruncher = new Cruncher(options);
-            cruncher.Crunch(26);
-            Console.WriteLine("Done.");
-            Console.ReadKey();
-            return;
-#endif
-
-            cruncher = new Cruncher(options);
+            Cruncher cruncher = new Cruncher(options);
 
             for(int i = options.MinInit; i != options.MaxInit + 1; i++) {
                 cruncher.Crunch(i);
