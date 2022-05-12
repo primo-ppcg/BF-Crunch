05accfb7eb6eeb2cb0b0f7ccf64ce12e68a9386c
primo-ppcg <mike.tryczak@gmail.com>
2022-05-12 17:11:34 +0700

    Update Node.cs

--- src/Node.cs
+++ src/Node.cs
@@ -1,18 +1,24 @@
-﻿using System;
+using System;
 
 namespace ppcg {
-    public struct Node {
+    public struct Node : IEquatable<Node> {
 
         public int Pointer;
         public int Cost;
+        public bool Rolling;
 
-        public Node(int pointer, int cost) {
+        public Node(int pointer, int cost, bool rolling = false) {
             this.Pointer = pointer;
             this.Cost = cost;
+            this.Rolling = rolling;
         }
 
         public override string ToString() {
-            return string.Format("({0}, {1})", this.Pointer, this.Cost);
+            return string.Format("({0} {1})", this.Pointer, this.Cost);
+        }
+
+        public bool Equals(Node other) {
+            return this.Pointer == other.Pointer;
         }
 
     }
